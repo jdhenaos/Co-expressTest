f05af5f172374c135443627bbd783878faf47277
JuanHenao <judhenaosa@unal.edu.co>
2016-02-19 23:36:11 -0500

    .

--- CoExpressionTest.R
+++ CoExpressionTest.R
@@ -35,3 +35,11 @@ gpl3 <- scan(dir(".")[grep("GPL570",dir("."))], comment.char = "!",
 head(as.data.frame(gpl3[grep("[#|^]",gpl3,invert = T)], sep = "t"))
 
 head("./GPL570.soft")
+
+gpl <- getGEO(filename = "GPL570.soft")
+colnames(Table(gpl))
+table(Table(gpl)$"Gene Symbol")
+write(table(Table(gpl)$"Gene Symbol"), file = "output")
+paste(Table(gpl)$"Gene Symbol"[1],":",table(Table(gpl)$"Gene Symbol"[1]))
+
+
